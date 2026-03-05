b9abecb9e9858034be58b23de5ef6652b277b366
tay-create <williamswilll14@gmail.com>
2026-03-05 17:36:11 -0300

    config paletes 2

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import AppRouter from './routes/AppRouter';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <AppRouter />
-);
--- src/services/index.js
+++
@@ -1,9 +0,0 @@
-/**
- * Exportação centralizada de todos os serviços
- * Uso: import { authService, operacaoService } from '@/services'
- */
-
-export { default as api } from './apiService';
-export { default as authService } from './authService';
-export { default as operacaoService } from './operacaoService';
-export { authService as auth } from './apiService'; // Manter compatibilidade
